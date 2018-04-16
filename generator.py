258becc86720d152e8ba964b0945827f7aa185b3
Michael <username.jones.xx@gmail.com>
2018-04-16 13:14:00 -0500

    Added octaves lacunarity & persitence to modify frequency and amplitude

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+from sklearn import preprocessing
 from opensimplex import OpenSimplex
 from PIL import Image
 
@@ -5,21 +6,56 @@ width = 640
 height = 480
 scale = 25
 
+octaves = 3
+persistence = .5
+lacunarity = 2
+
 def main():
-    mapNoiseGrid = generateNoiseGrid(width,height,scale)
+    mapNoiseGrid = generateNoiseGrid(width,height,scale,octaves,persistence,lacunarity)
     generateImg(mapNoiseGrid)
 
-def generateNoiseGrid(width,height,scale):
+def generateNoiseGrid(width,height,scale,octaves,persistence,lacunarity):
     noise = [[r for r in range(width)] for i in range(height)]
+
+    maxNoiseHeight = 0
+    minNoiseHeight = 0
 
     simplex = OpenSimplex()
     for y in range(0, height):
         for x in range(0, width):
-            samplex = x/scale
-            sampley = y/scale
-            noise[y][x] = simplex.noise2d(samplex,sampley)
 
-    return noise
+            amplitude = 1
+            frequency = 1
+            noiseHeight = 0
+
+            for o in range (0, octaves):
+                samplex = x/scale * frequency
+                sampley = y/scale * frequency
+
+                simplexValue = simplex.noise2d(samplex,sampley) #* 2 - 1
+                noiseHeight += simplexValue * amplitude
+
+                amplitude *= persistence
+                frequency *= lacunarity
+
+
+            if noiseHeight > maxNoiseHeight:
+                maxNoiseHeight = noiseHeight
+            elif noiseHeight < minNoiseHeight:
+                minNoiseHeight = noiseHeight
+
+
+            noise[y][x] = noiseHeight
+
+    noiseScaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+    noiseScaled = noiseScaler.fit_transform(noise)
+
+    """
+    for y in range(0, height):
+        for x in range(0, width):
+            noise[y][x] =
+    """
+    return noise #Scaled
 
 def generateImg(noiseGrid):
     im = Image.new('L', (width, height))

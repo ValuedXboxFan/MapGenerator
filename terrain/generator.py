709cb0e35c39f0ec1c0e908dc402b8527c701544
Michael <username.jones.xx@gmail.com>
2018-04-24 15:54:59 -0500

    Cleaned up py scripts

--- terrain/generator.py
+++ terrain/generator.py
@@ -2,7 +2,7 @@ import random
 from opensimplex import OpenSimplex
 from PIL import Image
 
-width = 1000
+width = 2000
 height = 1000
 scale = 185
 
@@ -47,16 +47,8 @@ def generateNoiseGrid(width,height,scale,octaves,persistence,lacunarity):
 
 
             noiseGrid[y][x] = noiseHeight
-            noiseGrid[y][x] = combined(x, y)
             noiseGrid[y][x] = (noiseGrid[y][x]-minNoiseHeight)/(maxNoiseHeight-minNoiseHeight)
 
-
-    """
-    for y in range(0, height):
-        for x in range(0, width):
-            #noiseGrid[y][x] = combined (noiseGrid[x], noiseGrid[y])
-            noiseGrid[y][x] = (noiseGrid[y][x]-minNoiseHeight)/(maxNoiseHeight-minNoiseHeight)
-    """
     return noiseGrid
 
 class TerrainType:
@@ -66,11 +58,11 @@ class TerrainType:
         self.color = color
 
 regions = [TerrainType("Water Deep", .3, (0, 100, 200)),
-            TerrainType("Water Shallow", .4, (0, 128, 255)),
-            TerrainType("Sand", .45, (255, 230, 95)),
-            TerrainType("Grass", .65, (50, 220, 0)),
-            TerrainType("Grass 2", .7, (35, 145, 0)),
-            TerrainType("Mountain1", .8, (140, 140, 140)),
+            TerrainType("Water Shallow", .45, (0, 128, 255)),
+            TerrainType("Sand", .5, (255, 230, 95)),
+            TerrainType("Grass", .7, (50, 190, 0)),
+            TerrainType("Grass 2", .75, (35, 145, 0)),
+            TerrainType("Mountain1", .85, (140, 140, 140)),
             TerrainType("Mountain2", .95, (100, 100, 100)),
             TerrainType("Snow", 1, (255, 255, 255))]
 
@@ -78,6 +70,7 @@ regions = [TerrainType("Water Deep", .3, (0, 100, 200)),
 def generateImg(noiseGrid):
     colorMap = []
 
+    #color
     im = Image.new('RGB', (width, height))
     for y in range(0, height):
         for x in range(0, width):
@@ -95,6 +88,7 @@ def generateImg(noiseGrid):
 
 
     """
+    #Black and White
     im = Image.new('L', (width, height))
     for y in range(0, height):
         for x in range(0, width):
@@ -107,40 +101,6 @@ def generateImg(noiseGrid):
     im.show()
     return
     """
-
-
-
-
-def simple_curve(x, y):
-    value = (simplex.noise2d(x,y))
-    start = 0.4
-    end = 0.6
-    if value < start:
-        return 0.0
-    if value > end:
-        return 1.0
-    return (value - start) * (1/(end-start))
-
-def plains(x, y):
-    value = (simplex.noise2d(x,y))
-    value = value**0.25
-    value = value - 0.6
-    return value
-
-def mountains (x, y):
-    value = simplex.noise2d(x*0.6, y * 3.0)
-    return value * 2.0
-
-def combined (x, y):
-    m = mountains(x, y)
-    p = plains(x, y)
-    w = simple_curve(x, y)
-    return (p * w) + (m * (1 - w))
-
-
-
-
-
 
 
 
